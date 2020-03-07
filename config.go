332d48dd332e19a148f97207a8c1b46be3ddc150
Parnic <github@parnic.com>
2020-03-07 10:54:59 -0600

    Documented config properties

--- config.go
+++ config.go
@@ -2,7 +2,10 @@ package precompiler
 
 // Config holds information on how to run the compiler
 type Config struct {
-	Files     []string
-	Minify    bool
+	// Files is a list of files to precompile together
+	Files []string
+	// Minify specifies whether minification should happen along with concatenation
+	Minify bool
+	// OutputDir if specified will cause the result to be written to this directory
 	OutputDir string
 }
